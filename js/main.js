8b7162952e2e00c4c7f3cb2d72d016dec7796041
coreavparis2020m <coreavparis2020m@gmail.com>
2020-05-18 13:04:25 +0200

    initial

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// Acceso a elementos
+
+// Por su atributo id
+
+let mainTitle = document.getElementById('main-title');
+
+mainTitle.style.color = 'crimson';
+
+console.log(mainTitle.style);
+
+// Por su clase CSS
+
+let importants = document.getElementsByClassName('important');
+
+console.log(importants);
+
+for (i = 0; i < importants.length; i++) {
+    importants[i].style.color = 'white';
+    importants[i].style.backgroundColor = 'brown';
+    importants[i].style.padding = '20px';
+}
