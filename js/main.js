fea4e2d5b821a458993af2f3281d5488b9ab1756
coreavparis2020m <coreavparis2020m@gmail.com>
2020-05-19 09:47:29 +0200

    dom

--- js/main.js
+++ js/main.js
@@ -19,3 +19,54 @@ for (i = 0; i < importants.length; i++) {
     importants[i].style.backgroundColor = 'brown';
     importants[i].style.padding = '20px';
 }
+
+// Por su etiqueta
+
+let links = document.getElementsByTagName('a');
+
+for (i = 0; i < links.length; i++) {
+    links[i].style.textDecoration = 'none';
+    links[i].style.color = 'brown';
+}
+
+// Por cualquier selector incluyendo combinaciones
+
+let fruits = document.querySelectorAll('.fruits li');
+
+console.log(fruits[0].innerText);
+
+for (i = 0; i < fruits.length; i++) {
+    if (fruits[i].innerText === 'Naranjas') {
+        fruits[i].style.color = 'orange'
+    }
+    if (fruits[i].innerText === 'Peras') {
+        fruits[i].style.color = 'green'
+    }
+    if (fruits[i].innerText === 'Manzanas') {
+        fruits[i].style.color = 'gold'
+    }
+}
+
+// Acceso a ascendientes/descendientes
+
+let fruitsList = document.querySelectorAll('.fruits');
+
+let fruitsElements = fruitsList[0].children;  // Propiedad que devuelve los hijos de un nodo
+
+console.log(fruitsElements);
+
+let volkswagenElement = document.getElementById('vw');
+
+let vehicles = volkswagenElement.parentElement; // Propiedad que devuelve el elemento padre de un nodo
+
+console.log(vehicles);
+
+// Crear y añadir elementos en el DOM
+
+let newBrand = document.createElement('p');  // Crea elemento en el dom pasándole la etiqueta
+
+newBrand.innerHTML = 'Ferrari';
+
+// vehicles.appendChild(newBrand); Añade un elemento como hijo al final
+
+vehicles.insertBefore(newBrand, volkswagenElement); // Añade el primer parámetro como hijo delante del segundo parámetro
